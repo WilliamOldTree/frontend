dc14f6503a6cd244611da33254cbbbc9e1ebb077
WilliamOldTree <denis.barbosa@fatec.sp.gov.br>
2021-11-18 12:31:25 -0300

    Inserção do carrousel no site Pichau

--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,38 +1,47 @@
-let nota =10
+// let nota =10
 
-switch (nota){
-    case 10:
-    case 9:
-        console.log("Melhores alunos")
-        break;
-    case 8:
-    case 7:
-	    console.log("muito bom")
-	    break;
-    case 6:
-    case 5:
-	    console.log("Ufa voce passou")
-	    break;
-    case 4:
-    case 3:
-	    console.log("Recuperação")
-	    break;
-    case 2:
-    case 1:
-    case 0:
-	    console.log("Reprovado")
-	    break;
-	default:
-	    console.log("Nota invalida")
-}
+// switch (nota){
+//     case 10:
+//     case 9:
+//         console.log("Melhores alunos")
+//         break;
+//     case 8:
+//     case 7:
+// 	    console.log("muito bom")
+// 	    break;
+//     case 6:
+//     case 5:
+// 	    console.log("Ufa voce passou")
+// 	    break;
+//     case 4:
+//     case 3:
+// 	    console.log("Recuperação")
+// 	    break;
+//     case 2:
+//     case 1:
+//     case 0:
+// 	    console.log("Reprovado")
+// 	    break;
+// 	default:
+// 	    console.log("Nota invalida")
+// }
 
-let valor = 10;
-switch (true) {
-    case (valor == 10 || valor == 9): console.log("Melhores alunos"); break;
-    case (valor ==  8 || valor == 7): console.log("Muito bom"); break;
-    case (valor ==  6 || valor == 5): console.log("Ufa voce passou"); break;
-    case (valor ==  4 || valor == 3): console.log("Recuperação"); break;
-    case (valor ==  2 || valor == 1 || valor == 0): console.log("Reprovado"); break;
-    default: console.log("Nota invalida");
-}
+// let valor = 10;
+// switch (true) {
+//     case (valor == 10 || valor == 9): console.log("Melhores alunos"); break;
+//     case (valor ==  8 || valor == 7): console.log("Muito bom"); break;
+//     case (valor ==  6 || valor == 5): console.log("Ufa voce passou"); break;
+//     case (valor ==  4 || valor == 3): console.log("Recuperação"); break;
+//     case (valor ==  2 || valor == 1 || valor == 0): console.log("Reprovado"); break;
+//     default: console.log("Nota invalida");
+// }
 	    
+
+function alerta(){
+    alert("Ola")
+    
+}
+
+function altFundo(){
+
+}
